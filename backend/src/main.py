a45c8660afac3f2784475e475a51be22b730e3bc
Gabriel Vieira <gabriel.qmv@icloud.com>
2024-07-24 17:54:09 -0300

    chore: Update Flask app run command to bind to all network interfaces

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from routes import scraper_bp
 from flask_cors import CORS
+from routes import scraper_bp
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(scraper_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
